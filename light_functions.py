55cf9081cdfcd90ea0bd1a5c33c2a73c6ed34b14
Eder Ollora <eoza@fotonik.dtu.dk>
2018-11-19 16:05:55 +0100

    little fixes

--- light_functions.py
+++ light_functions.py
@@ -128,6 +128,7 @@ def randomshow(*, pin_numbers: list, iterations=1, sleep=2) -> None:
 def allonshow(*, pin_numbers: list, iterations=1, sleep=1) -> None:
 
     for pin in pin_numbers:
+        print(pin)
         on(pin)
     time.sleep(sleep)
 
@@ -174,7 +175,7 @@ def light_show():
         sleep=random_sleep)
 
 def random_show():
-    print pin_numbers
+    print(pin_numbers)
     randomshow(pin_numbers=pin_numbers)
 
 def allon_show():
